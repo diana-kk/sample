7356fc2f654f57304a0c0b54564ca93e3d3b4780
diana-kk <61671420+diana-kk@users.noreply.github.com>
2020-09-11 17:26:21 +0900

    Add files via upload

---
+++ korean02.js
@@ -0,0 +1,158 @@
+var SVG = Snap('#my-svg');
+
+// 최상위 그룹
+var Paper = SVG.g();
+
+//이미지
+var hidari = Paper.image('img/hidari.png', 0, 0, 21, 23).toDefs();
+var migi = Paper.image('img/migi.png', 0, 0, 21, 23).toDefs();
+var direction = Paper.image('img/direction.png', 0, 0, 25, 30).toDefs();
+var fox = Paper.image('img/fox.png', 0, 0, 105, 150).toDefs();
+var choice01 = Paper.image('img/choiceB01.png', 0, 0, 115, 63).toDefs();
+var choice02 = Paper.image('img/choiceB02.png', 0, 0, 118, 59).toDefs();
+var choice03 = Paper.image('img/choiceB03.png', 0, 0, 118, 60).toDefs();
+var choice04 = Paper.image('img/choiceB04.png', 0, 0, 114, 60).toDefs();
+
+//SVG 외곽선
+Paper.rect(0, 0, 360, 640).attr({
+  'stroke': 'gray',
+  'fill': 'none',
+  'rx': 6,
+  'ry': 6
+});
+
+// 메뉴 박스
+var topBox = Paper.g();
+topBox.path('M1 40 L359 40 L359 0 Q358 1 358 1 L1 1').attr({
+  'fill': '#E8EBEE'
+});
+topBox.text(180, 27, '낱말 읽기').attr({
+  'font-size': 18,
+  'text-anchor': 'middle'
+
+});
+
+hidari.use().transform('t10, 9').appendTo(topBox).click(handlerPre).attr({
+  'cursor': 'pointer'
+});
+
+function handlerPre() {
+  location.replace('korean01.html');
+}
+
+migi.use().transform('t325, 9').appendTo(topBox).click(handlerNext).attr({
+  'cursor': 'pointer'
+});
+
+function handlerNext() {
+  location.replace('korean03.html');
+}
+
+// 상단
+var topArea = Paper.g();
+
+direction.use().transform('t12, 55').appendTo(topArea);
+topBox.text(44, 80, '그림에 알맞은 낱말을 고르세요.').attr({
+  'font-size': 18,
+});
+
+fox.use().transform('t125, 125').appendTo(topArea);
+
+var bottomArea = Paper.g();
+
+//첫번째 보기
+bottomArea.rect(20, 335, 150, 100).click(pop02).attr({
+  'fill': '#FCF8E7',
+  'stroke': '#82C71D',
+  'stroke-width': 2,
+  'stroke-dasharray': '8 8',
+  'rx': 5,
+  'ry': 5,
+  'cursor': 'pointer'
+});
+choice01.use().transform('t34, 353').appendTo(bottomArea).click(pop02).attr({
+  'cursor': 'pointer'
+});
+
+//두번째 보기
+bottomArea.rect(190, 335, 150, 100).click(pop01).attr({
+  'fill': '#FCF8E7',
+  'stroke': '#82C71D',
+  'stroke-width': 2,
+  'stroke-dasharray': '8 8',
+  'rx': 5,
+  'ry': 5,
+  'cursor': 'pointer'
+});
+choice02.use().transform('t206, 355').appendTo(bottomArea).click(pop01).attr({
+  'cursor': 'pointer'
+});
+
+//세번째 보기
+bottomArea.rect(20, 470, 150, 100).click(pop02).attr({
+  'fill': '#FCF8E7',
+  'stroke': '#82C71D',
+  'stroke-width': 2,
+  'stroke-dasharray': '8 8',
+  'rx': 5,
+  'ry': 5,
+  'cursor': 'pointer'
+});
+choice03.use().transform('t37, 490').appendTo(bottomArea).click(pop02).attr({
+  'cursor': 'pointer'
+});
+
+//네번째 보기
+bottomArea.rect(190, 470, 150, 100).attr({
+  'fill': '#FCF8E7',
+  'stroke': '#82C71D',
+  'stroke-width': 2,
+  'stroke-dasharray': '8 8',
+  'rx': 5,
+  'ry': 5,
+  'cursor': 'pointer'
+});
+choice04.use().transform('t205, 490').appendTo(bottomArea).click(pop02).attr({
+  'cursor': 'pointer'
+});
+
+//정오답 확인
+function pop01() {
+  var maru = Paper.circle(180, 445, 120).click(pop02).attr({
+    'stroke': 'tomato',
+    'stroke-width': 35,
+    'fill': 'none',
+    'opacity': 0.5
+  });
+
+  var audio1 = new Audio('aud/code01.mp3');
+  audio1.play();
+
+  setTimeout(function() {
+    maru.remove();
+    location.replace('korean03.html');
+  }, 1500);
+}
+
+function pop02() {
+  var fdEl1 = Paper.line(60, 330, 300, 570).attr({
+    'stroke': '#bbbbbb',
+    'stroke-width': 30,
+    'stroke-linecap': 'round',
+    'opacity': 0.5
+  });
+  var fdEl2 = Paper.line(300, 330, 60, 570).attr({
+    'stroke': '#bbbbbb',
+    'stroke-width': 30,
+    'stroke-linecap': 'round',
+    'opacity': 0.5
+  });
+  var batsu = Paper.g(fdEl1, fdEl2);
+
+  var audio2 = new Audio('aud/code02.mp3');
+  audio2.play();
+
+  setTimeout(function() {
+    batsu.remove();
+  }, 1500);
+}
---
+++ math01.js
@@ -0,0 +1,163 @@
+var SVG = Snap('#my-svg');
+
+// 최상위 그룹
+var Paper = SVG.g();
+
+//이미지
+var hidari = Paper.image('img/hidari.png', 0, 0, 21, 23).toDefs();
+var migi = Paper.image('img/migi.png', 0, 0, 21, 23).toDefs();
+var direction = Paper.image('img/direction.png', 0, 0, 25, 30).toDefs();
+
+//SVG 외곽선
+Paper.rect(0, 0, 360, 640).attr({
+  'stroke': 'gray',
+  'fill': 'none',
+  'rx': 6,
+  'ry': 6
+});
+
+// 메뉴 박스
+var topBox = Paper.g();
+topBox.path('M1 40 L359 40 L359 0 Q358 1 358 1 L1 1').attr({
+  'fill': '#E8EBEE'
+});
+topBox.text(180, 27, '덧셈과 뺄셈').attr({
+  'font-size': 18,
+  'text-anchor': 'middle'
+});
+
+hidari.use().transform('t10, 9').appendTo(topBox).click(handlerPre).attr({
+  'cursor': 'pointer'
+});
+
+function handlerPre() {
+  location.replace('math00.html');
+}
+
+migi.use().transform('t325, 9').appendTo(topBox).click(handlerNext).attr({
+  'cursor': 'pointer'
+});
+
+function handlerNext() {
+  location.replace('math02.html');
+}
+
+// 상단
+var topArea = Paper.g();
+
+direction.use().transform('t12, 55').appendTo(topArea);
+topBox.text(44, 80, '다음과 같은 수를 나타내는 것을 고르세요.').attr({
+  'font-size': 18,
+});
+
+var bottomArea = Paper.g();
+
+bottomArea.text(180, 215, '6').attr({
+  'font-size': 110,
+  'text-anchor': 'middle'
+});
+
+//첫번째 보기
+bottomArea.rect(40, 270, 280, 65).click(pop02).attr({
+  'fill': '#F6FEF5',
+  'stroke': 'skyblue',
+  'stroke-width': 2,
+  'stroke-dasharray': '8 8',
+  'rx': 5,
+  'ry': 5,
+  'cursor': 'pointer'
+});
+bottomArea.text(180, 322, '3 + 1').click(pop02).attr({
+  'font-size': 50,
+  'text-anchor': 'middle',
+  'cursor': 'pointer'
+});
+
+//두번째 보기
+bottomArea.rect(40, 355, 280, 65).click(pop02).attr({
+  'fill': '#F6FEF5',
+  'stroke': 'skyblue',
+  'stroke-width': 2,
+  'stroke-dasharray': '8 8',
+  'rx': 5,
+  'ry': 5,
+  'cursor': 'pointer'
+});
+bottomArea.text(180, 407, '2 + 3').click(pop02).attr({
+  'font-size': 50,
+  'text-anchor': 'middle',
+  'cursor': 'pointer'
+});
+
+//세번째 보기
+bottomArea.rect(40, 440, 280, 65).click(pop01).attr({
+  'fill': '#F6FEF5',
+  'stroke': 'skyblue',
+  'stroke-width': 2,
+  'stroke-dasharray': '8 8',
+  'rx': 5,
+  'ry': 5,
+  'cursor': 'pointer'
+});
+bottomArea.text(180, 492, '3 + 3').click(pop01).attr({
+  'font-size': 50,
+  'text-anchor': 'middle',
+  'cursor': 'pointer'
+});
+
+//네번째 보기
+bottomArea.rect(40, 525, 280, 65).click(pop02).attr({
+  'fill': '#F6FEF5',
+  'stroke': 'skyblue',
+  'stroke-width': 2,
+  'stroke-dasharray': '8 8',
+  'rx': 5,
+  'ry': 5,
+  'cursor': 'pointer'
+});
+bottomArea.text(180, 577, '4 + 3').click(pop02).attr({
+  'font-size': 50,
+  'text-anchor': 'middle',
+  'cursor': 'pointer'
+});
+
+//정오답 확인
+function pop01() {
+  var maru = Paper.circle(180, 425, 120).attr({
+    'stroke': 'tomato',
+    'stroke-width': 35,
+    'fill': 'none',
+    'opacity': 0.5
+  });
+
+  var audio1 = new Audio('aud/code01.mp3');
+  audio1.play();
+
+  setTimeout(function() {
+    maru.remove();
+    location.replace('math02.html');
+  }, 1500);
+}
+
+function pop02() {
+  var fdEl1 = Paper.line(60, 310, 300, 550).attr({
+    'stroke': '#bbbbbb',
+    'stroke-width': 30,
+    'stroke-linecap': 'round',
+    'opacity': 0.5
+  });
+  var fdEl2 = Paper.line(300, 310, 60, 550).attr({
+    'stroke': '#bbbbbb',
+    'stroke-width': 30,
+    'stroke-linecap': 'round',
+    'opacity': 0.5
+  });
+  var batsu = Paper.g(fdEl1, fdEl2);
+
+  var audio2 = new Audio('aud/code02.mp3');
+  audio2.play();
+
+  setTimeout(function() {
+    batsu.remove();
+  }, 1500);
+}
